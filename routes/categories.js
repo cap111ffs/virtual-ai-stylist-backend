e3eeda2df60f7c00e4b9be66e5b41ea128ddb04a
cap111ffs <mamed.bayramov.04.04@mail.ru>
2023-03-16 20:22:23 +0500

    Изменил структуру кода

--- routes/categories.js
+++ routes/categories.js
@@ -1,5 +1,5 @@
 const router = require("express").Router()
-const Category = require("../model/Category")
+const Category = require("../model/categoryj")
 
 router.post("/", async (req, res) => {
     const newCat = new Category(req.body)
