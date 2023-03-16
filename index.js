b8d50498501341fb744ff46b04b1a229361d0109
cap111ffs <mamed.bayramov.04.04@mail.ru>
2023-03-16 19:30:21 +0500

    Изменил структуру проекта, удалил ненужные папки и подредактировал код

---
+++ index.js
@@ -0,0 +1,48 @@
+const express = require("express")
+const app = express()
+const dotenv = require("dotenv")
+const multer = require("multer")
+const path = require("path")
+
+const mongoose = require("mongoose")
+const authRoute = require("./routes/auth")
+const authUser = require("./routes/user")
+const authPost = require("./routes/posts")
+const authCat = require("./routes/categories")
+dotenv.config()
+app.use(express.json())
+app.use("/images", express.static(path.join(__dirname, "./images")))
+
+mongoose
+.connect(process.env.CONNECTION_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,   
+})
+.then(console.log("Connected to MongoDb"))
+.catch((err) => console.log(err))
+
+const storage = multer.diskStorage({
+    destination: (req, file, callb) => {
+      callb(null, "images")
+    },
+    filename: (req, file, callb) => {
+      callb(null, "file.jpg")
+    }
+  })
+
+const upload = multer({ storage: storage })
+
+app.post("/upload", upload.single("file"), (req, res) => {
+    res.status(200).json("File has been uploaded")
+})
+
+app.use("/auth", authRoute)
+app.use("/users", authUser)
+app.use("/posts", authPost)
+app.use("/category", authCat)
+
+
+
+app.listen("5000", () => {
+    console.log("Backend running...")
+})
---
+++ model/user.js
@@ -0,0 +1,27 @@
+const mongoose = require("mongoose")
+
+const UserSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        require: true,
+        unique: true,
+    },
+    phoneNumber: {
+        type: String,
+        require: true,
+        unique: true,
+    },
+    password: {
+        type: String,
+        require: true,
+    },
+    profilePic: {
+        type: String,
+        default: "",
+    },
+},
+    {
+        timestamps: true,
+    }
+)
+module.exports = mongoose.model("User", UserSchema)
