fb09269bfac47b903b5171b4da6e3c570e84758e
cap111ffs <mamed.bayramov.04.04@mail.ru>
2023-03-17 20:03:49 +0500

    Изменил структуру ввода постов

--- model/postj.js
+++ model/postj.js
@@ -14,7 +14,7 @@ const PostSchema = new mongoose.Schema({
         type: String,
         require: false,
     },
-    username: {
+    userName: {
         type: String,
         require: true,
     },
