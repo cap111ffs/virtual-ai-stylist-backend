3d638fc66442282c66152781a4467c9eebdb027d
andreybyrsa <byrsa2004@bk.ru>
2023-03-24 11:15:55 +0500

    Переделал роутер авторизации пользователя

--- routes/auth.js
+++ routes/auth.js
@@ -1,22 +1,26 @@
 const router = require('express').Router()
 const User = require('../model/UserModel')
-const bcrypt = require('bcrypt')
-const smsaerov2 = require('smsaero-v2')
+const dotenv = require('dotenv')
+const sendOtpMessage = require('../utils/sendOtpMessage')
+dotenv.config()
+
+let userId
+let otpCode
+
 router.post('/register', async (req, res) => {
   try {
-    const salt = await bcrypt.genSalt(10)
-    const hashedPass = await bcrypt.hash(req.body.passWord, salt)
-
     const newUser = new User({
       userName: req.body.userName,
       phoneNumber: req.body.phoneNumber,
-      passWord: hashedPass,
-      profilePic: req.body.profilePic,
     })
 
     const { _doc: user } = await newUser.save()
     const { _id, ...currentUser } = user
-    currentUser.id = _id
+    userId = _id
+
+    otpCode = 'hell nahhh blud, what u waffling bout' // here is generate code like generateCode()
+
+    sendOtpMessage(req.body.phoneNumber, otpCode)
 
     res.status(200).json(currentUser)
   } catch (error) {
@@ -30,31 +34,29 @@ router.post('/login', async (req, res) => {
 
     !user && res.status(400).json('No user!')
 
-    const validate = bcrypt.compare(req.body.passWord, user.passWord)
-
-    !validate && res.status(400).json('Wrong Credentials!')
-
     const { _id, ...currentUser } = user._doc
-    currentUser.id = _id
+    userId = _id
 
-    const client = new smsaerov2.Client(
-      'cap111ffsinho@gmail.com',
-      'NEFI_F6sufBEqm7fSfcDTbPzgQL5S4cf',
-      { timeout: 10 },
-    )
-    ;(async function () {
-      const res = await client.send(
-        new smsaerov2.Message({
-          sign: 'SMS Aero',
-          number: '79829258384', // num or [num1, num2, ...numN]
-          text: 'Test message',
-        }),
-      )
-      console.log(res)
-    })()
+    otpCode = '54321' // here is generate code like generateCode()
+
+    sendOtpMessage(req.body.phoneNumber, otpCode)
+
     res.status(200).json(currentUser)
   } catch (error) {
     res.status(500).json(error)
   }
 })
+
+router.post('/verify', async (req, res) => {
+  try {
+    if (req.body.code === otpCode) {
+      res.status(200).json({ userId })
+    } else {
+      throw new Error('Invalid code')
+    }
+  } catch (error) {
+    res.status(500).json(error)
+  }
+})
+
 module.exports = router
