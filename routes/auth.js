5f0b6b1a931c9e1085313f95f65f1a0301d8f41e
cap111ffs <mamed.bayramov.04.04@mail.ru>
2023-03-24 10:30:58 +0500

    Добавил вход по коду с номера телефона

--- routes/auth.js
+++ routes/auth.js
@@ -1,7 +1,7 @@
 const router = require('express').Router()
 const User = require('../model/UserModel')
 const bcrypt = require('bcrypt')
-
+const smsaerov2 = require('smsaero-v2')
 router.post('/register', async (req, res) => {
   try {
     const salt = await bcrypt.genSalt(10)
@@ -37,10 +37,24 @@ router.post('/login', async (req, res) => {
     const { _id, ...currentUser } = user._doc
     currentUser.id = _id
 
+    const client = new smsaerov2.Client(
+      'cap111ffsinho@gmail.com',
+      'NEFI_F6sufBEqm7fSfcDTbPzgQL5S4cf',
+      { timeout: 10 },
+    )
+    ;(async function () {
+      const res = await client.send(
+        new smsaerov2.Message({
+          sign: 'SMS Aero',
+          number: '79829258384', // num or [num1, num2, ...numN]
+          text: 'Test message',
+        }),
+      )
+      console.log(res)
+    })()
     res.status(200).json(currentUser)
   } catch (error) {
     res.status(500).json(error)
   }
 })
-
 module.exports = router
