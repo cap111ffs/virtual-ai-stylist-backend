128b04c58d03cdc58aa0deb26fdd2fd50402cf03
andreybyrsa <byrsa2004@bk.ru>
2023-03-24 11:16:37 +0500

    Добавил отдельную папку для функций

---
+++ utils/sendOtpMessage.js
@@ -0,0 +1,14 @@
+const smsaerov2 = require('smsaero-v2')
+
+const sendOtpMessage = async (phoneNumber, message) => {
+  const client = new smsaerov2.Client(process.env.SMSAERO_LOGIN_URL, process.env.SMSAERO_API_KEY)
+  const res = await client.send(
+    new smsaerov2.Message({
+      sign: 'SMS Aero',
+      number: phoneNumber,
+      text: message,
+    }),
+  )
+}
+
+module.exports = sendOtpMessage
