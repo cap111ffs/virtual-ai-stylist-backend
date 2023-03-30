b1bb58000dd730731b913cbba3efda23bac1b397
andreybyrsa <byrsa2004@bk.ru>
2023-03-30 17:07:45 +0500

    Добавил удаление кода после подтсверждения верификации

--- routes/auth.js
+++ routes/auth.js
@@ -58,6 +58,8 @@ router.post('/verify', async (req, res) => {
     const otpCode = await OtpCode.findOne({ phoneNumber: req.body.phoneNumber });
 
     if (req.body.code === otpCode.code) {
+      deleteCurrentOtpCode(otpCode.id, 240);
+
       return res.status(200).json({ id: otpCode.id });
     }
 
